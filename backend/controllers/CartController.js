defc1a4c11c2357d2ca82940813659f38f66bb57
abinjos307@gmail.com <abinjosofficial307@gmail.com>
2024-07-21 02:25:39 +0530

    Cart Integrated with frontend

--- backend/controllers/CartController.js
+++ backend/controllers/CartController.js
@@ -2,17 +2,48 @@ import userModel from '../models/UserModel.js'
 
 //add cart
 const addItem = async (req,res) => {
-    res.send('hi')
+    try {
+        const userData = await userModel.findOne({_id:req.body.userId});
+        const cartData = await userData.cartData
+        if(!cartData[req.body.itemId]){
+            cartData[req.body.itemId] = 1
+        }else{
+            cartData[req.body.itemId] += 1
+        }
+        await userModel.findOneAndUpdate({_id:req.body.userId},{cartData})
+        res.json({success:true,message:"Item Added to cart"})
+    } catch (error) {
+        res.json({success:false,message:"Item Not Added to cart"})
+        console.log(error);        
+    }
 }
 
 //remove item
 const removeItem = async (req,res) => {
-
+    try {
+        const userData = await userModel.findOne({_id:req.body.userId})
+        const cartData = await userData.cartData;
+        if(cartData[req.body.itemId]>0){
+            cartData[req.body.itemId] -= 1;
+        }
+        await userModel.findOneAndUpdate({_id:req.body.userId},{cartData})
+        res.json({success:true,message:"Item Deleted form cart"})
+    } catch (error) {
+        res.json({success:false,message:"Item Not Deleted from cart"})
+        console.log(error);  
+    }
 }
 
 //get items
 const getItem = async (req,res) => {
-
+    try {
+        const userData = await userModel.findOne({_id:req.body.userId})
+        const cartData = await userData.cartData;
+        res.json({success:true,cartData})
+    } catch (error) {
+        res.json({success:false,message:"Error in fetching cart"})
+        console.log(error);  
+    }
 }
 
 export {addItem, removeItem, getItem};
--- backend/routes/CartRoute.js
+++ backend/routes/CartRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import authMiddleware from '../middleware/auth.js'
 import { addItem, removeItem, getItem } from '../controllers/CartController.js'
 
 const CartRoute = express.Router();
 
-CartRoute.post("/add",addItem)
-CartRoute.post("/get",getItem)
-CartRoute.post("/remove",removeItem)
+CartRoute.post("/add",authMiddleware,addItem)
+CartRoute.post("/get",authMiddleware,getItem)
+CartRoute.post("/remove",authMiddleware,removeItem)
 
 export default CartRoute;
